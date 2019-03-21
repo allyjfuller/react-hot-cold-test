65a90fd300dc405283242b801fad5b21c92dfc3d
allyjfuller <allyjfuller@gmail.com>
2019-03-21 10:21:32 -0400

    Shallow tests no errors

--- src/components/guess-list.test.js
+++ src/components/guess-list.test.js
@@ -5,7 +5,7 @@ import GuessList from './guess-list';
 
 describe('<GuessList />', () => {
     it('Renders without crashing', () => {
-        shallow(<GuessList />);
+        shallow(<GuessList guesses={[]} />);
     });
 
     
